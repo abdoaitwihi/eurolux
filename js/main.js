2be15b4a87d98a2a9df87a246c1ed1283c185357
abdoaitwihi <abdoaitwihi@gmail.com>
2022-01-25 14:24:51 +0100

    feat

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 $(document).ready(function () {
-  let headerHeight = $("header").outerHeight();
-  let windowHeight = $(window).height();
-  $(".menu").height(windowHeight - headerHeight);
-  $(".menu").height(windowHeight - headerHeight);
-  // $(".home-banner-inner").css("min-height", windowHeight - 300);
-  $(".home-banner-inner").css("min-height", windowHeight - headerHeight);
-
-  /* menu toggole */
-  $(".menu-toggler").click(function (e) {
-    e.preventDefault();
-    $(".menu").toggleClass("active");
-    $(".menu-icon").toggleClass("active");
-    $(".menu-toggler .menu-text").toggle();
-    $(".menu-toggler .close").toggle();
-  });
-
   /* carousel testimonials */
   let owlExperties = $(".owl-expertieses");
 
@@ -34,24 +18,6 @@ $(document).ready(function () {
       },
       1000: {
         items: 3,
-      },
-    },
-  });
-  $(".owl-solutions").owlCarousel({
-    // loop: true,
-    margin: 20,
-    // center: true,
-    nav: false,
-    dots: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 4,
       },
     },
   });
