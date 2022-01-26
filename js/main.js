dc617cf233674811f2186261314c6005526a31ab
abdoaitwihi <abdoaitwihi@gmail.com>
2022-01-26 17:30:39 +0100

    feat

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+  /* add class on scroll */
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+      $("header").addClass("scrolled");
+    } else {
+      $("header").removeClass("scrolled");
+    }
+  });
+
   $(".owl-brochures").owlCarousel({
     // loop: true,
     margin: 20,
@@ -15,6 +26,25 @@ $(document).ready(function () {
       },
       1000: {
         items: 3,
+      },
+    },
+  });
+  $(".owl-full-space").owlCarousel({
+    // loop: true,
+    margin: 20,
+    // center: true,
+    nav: true,
+    dots: false,
+    navText: ["<img src='assets/icons/back.svg' />", "<img src='assets/icons/back.svg' />"],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
       },
     },
   });
