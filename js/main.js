876ed97a39356028031b237841be7cac106619ef
abdoaitwihi <abdoaitwihi@gmail.com>
2022-02-28 16:58:22 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  /* carousel testimonials */
+  let owlExperties = $(".owl-expertieses");
+
+  owlExperties.owlCarousel({
+    screenLeft: true,
+    loop: false,
+    margin: 20,
+    center: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
